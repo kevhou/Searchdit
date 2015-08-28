5bb06cef35c4689ef9b32c663bcb216cdd3a0062
Kevin Hou <kevhou1995@gmail.com>
2015-08-28 15:22:42 -0400

    chore(style): margin fix

--- assets/scripts/about.jsx
+++ assets/scripts/about.jsx
@@ -7,23 +7,25 @@ var About = React.createClass({
   render: function() {
     return(
       <DocumentTitle title="About - Searchdit">
-        <div className="m-info">
-    			<a className="info-logo" href="#">
-    				<img className="img-responsive" alt="Searchdit" src="assets/images/logo1.png"/>
-    			</a>
+        <div id="content">
+          <div className="m-info">
+      			<a className="info-logo" href="#">
+      				<img className="img-responsive" alt="Searchdit" src="assets/images/logo1.png"/>
+      			</a>
 
-          <div className="info-body">
-            <div className="title">ABOUT</div>
-            <div className="body">Browse reddit at work without getting caught</div>
-            <div className="body">Built with ReactJs and React-Router</div>
-            <div className="body">Source code at <a target="_blank" href="https://github.com/kevhou/Searchdit">Github</a></div>
-          </div>
+            <div className="info-body">
+              <div className="title">ABOUT</div>
+              <div className="body">Browse reddit at work without getting caught</div>
+              <div className="body">Built with ReactJs and React-Router</div>
+              <div className="body">Source code at <a target="_blank" href="https://github.com/kevhou/Searchdit">Github</a></div>
+            </div>
 
-          <div className="m-footer">
-            <Link to="home">Home</Link>
-            <Link to="help">Help</Link>
-            <Link to="about">About</Link>
-            <Link to="search" params={{sort: "hot", splat:"/r/random"}}>Random</Link>
+            <div className="m-footer">
+              <Link to="home">Home</Link>
+              <Link to="help">Help</Link>
+              <Link to="about">About</Link>
+              <Link to="search" params={{sort: "hot", splat:"/r/random"}}>Random</Link>
+            </div>
           </div>
         </div>
       </DocumentTitle>
--- assets/scripts/help.jsx
+++ assets/scripts/help.jsx
@@ -7,22 +7,24 @@ var Help = React.createClass({
   render: function() {
     return(
       <DocumentTitle title="Help - Searchdit">
-        <div className="m-info">
-    			<a className="info-logo" href="#">
-    				<img className="img-responsive" alt="Searchdit" src="assets/images/logo1.png"/>
-    			</a>
+        <div id="content">
+          <div className="m-info">
+      			<a className="info-logo" href="#">
+      				<img className="img-responsive" alt="Searchdit" src="assets/images/logo1.png"/>
+      			</a>
 
-          <div className="info-body">
-            <div className="title">HELP</div>
-            <div className="body">Search as you would on reddit</div>
-            <div className="body">Search '/r/[subreddit]' to view posts of a certain subreddit</div>
-          </div>
+            <div className="info-body">
+              <div className="title">HELP</div>
+              <div className="body">Search as you would on reddit</div>
+              <div className="body">Search '/r/[subreddit]' to view posts of a certain subreddit</div>
+            </div>
 
-          <div className="m-footer">
-            <Link to="home">Home</Link>
-            <Link to="help">Help</Link>
-            <Link to="about">About</Link>
-            <Link to="search" params={{sort: "hot", splat:"/r/random"}}>Random</Link>
+            <div className="m-footer">
+              <Link to="home">Home</Link>
+              <Link to="help">Help</Link>
+              <Link to="about">About</Link>
+              <Link to="search" params={{sort: "hot", splat:"/r/random"}}>Random</Link>
+            </div>
           </div>
         </div>
       </DocumentTitle>
